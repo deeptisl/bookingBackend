9c9de93e6da4cca75c00459bfb02d98989b06bd8
Deepak Kumar <deepak@devcentrics.com>
2020-02-26 23:48:00 +0530

    first commit

---
+++ BookingBackend/Data/BookingSystemContext.cs
@@ -0,0 +1,22 @@
+﻿using BookingBackend.Modal;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingBackend.Data
+{
+    public class BookingSystemContext : DbContext
+    {
+        public BookingSystemContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<City> City { get; set; }
+        public DbSet<Driver> Driver { get; set; }
+    }
+}
